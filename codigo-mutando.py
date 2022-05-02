8bb69b0ba95462cc3dab39731e4c73052af2588e
driquelme-esri-chile <driquelme@esri.cl>
2022-05-02 09:16:48 -0400

    ordeando librerias

---
+++ codigo-mutando.py
@@ -0,0 +1,49 @@
+"""Codigo no funcional, falta que mute"""
+
+
+from asyncore import write
+import datetime
+import pandas as pd
+import locale
+import tkinter
+from tkinter import messagebox
+from tkinter import *
+from tkinter import ttk
+from docxtpl import DocxTemplate
+from itertools import groupby
+from operator import itemgetter
+from asyncore import write
+import datetime
+import pandas as pd
+import locale
+from openpyxl import load_workbook
+import pandas.io.formats.excel
+from openpyxl.styles.alignment import Alignment
+from openpyxl.styles.colors import Color
+from openpyxl.styles import PatternFill,Border,Side
+
+
+
+# This code is to hide the main tkinter window
+#win= Tk()
+
+#win.geometry("750x200")
+root = tkinter.Tk()
+root.withdraw()
+
+# Message Box
+messagebox.showinfo("Title", "Message")
+
+
+#addin .net c# 
+
+
+
+
+
+
+
+
+
+
+
