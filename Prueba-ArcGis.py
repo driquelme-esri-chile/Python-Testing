d8d86044794558b35ee858c1df059d50392bb425
driquelme-esri-chile <driquelme@esri.cl>
2022-05-05 12:46:06 -0400

    agregando comentarios a cada linea de codigo (Torpedo)

--- Prueba-ArcGis.py
+++ Prueba-ArcGis.py
@@ -21,7 +21,7 @@ def ScriptTool(capa):
     #el FID de cada una de las filas de la tabla y guardar dichos FID en el objeto 
     #creado anteriormente mediante la condicion "append."
     for row in arcpy.SearchCursor(capa):
-        selectedFeatures.append(row.FID)
+        selectedFeatures.append(row.getValue("FID"))
 
     #Se verifica el contenido del objeto creado, este debe contener los FID de 
     #cada una de las filas de toda la tabla
@@ -29,17 +29,35 @@ def ScriptTool(capa):
 
     #En este paso lo que se hace es modificar el objeto a un arreglo y 
     #transformarlos a String con el fin de poder manipularlos uno por uno
+    #Para lograr esto primero definimos que en cada recorrido entre x y selectFeatures
+    #estos se separen con ', ' (una coma y un espacio, con el fin de visualicar 
+    #de manera mas ordenada los nombres)
     srtFeatures = ', '.join(str(x) for x in selectedFeatures)
-
 
     #Se comprueba el arreglo creado
     arcpy.AddMessage(f'srtFeatures: {srtFeatures}')
 
     # Creo la query para obtener los features seleccionados de la capa.
     expression = """{0} in ({1})""".format(arcpy.AddFieldDelimiters(capa, 'FID'), srtFeatures)
-    
+
+
     #Se verifica la tupla creada .... investigar mas esta nueva transformacion de los datos
     arcpy.AddMessage(f'expression: {expression}')
+
+    #Se define una variable fija con el fin de usarla para completar ciertas funciones
+    capaAuxiliar = 'capa de testeo'
+    
+    arcpy.MakeFeatureLayer_management(capa, "lyr") 
+    arcpy.SelectLayerByAttribute_management("lyr", "NEW_SELECTION", where_clause=expression)
+    arcpy.CopyFeatures_management("lyr", capaAuxiliar)
+
+    
+    # Ordeno por el nombre la capa de seleccion
+    sort_fields = [["NOM_CON", "ASCENDING"]]
+    
+    arcpy.Sort_management(capa, capaAuxiliar, sort_fields)
+
+
 
 #Ejecutando codigo de importacion de datos de otros modulos, en este caso ArcGIS
 if __name__ == '__main__':
